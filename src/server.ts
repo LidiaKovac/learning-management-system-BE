6af6f2ddf9705ac8663d53a2dd8514de14a34758
Lidia Kovac <lidia.kovac1998@gmail.com>
2021-03-30 09:50:33 +0200

    adds cors configuration

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Router } from "express";
+import { Request } from "express-serve-static-core";
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require("cookie-parser")
@@ -6,7 +7,17 @@ const cookieParser = require("cookie-parser")
 const app = express()
 //do not declare db
 
-app.use(cors({credentials: true}))
+
+app.use(cors({
+    origin: [
+      process.env.FE_URI,
+      "http://localhost:3002/",
+      "http://localhost:3000/",
+    ],
+    credentials: true,
+    exposedHeaders: ["set-cookie"],
+  })
+);
 app.use(express.json())
 app.use(require("helmet")())
 
@@ -23,6 +34,8 @@ app.use("/files", filesRouter)
 app.use("/event", eventsRouter)
 app.use("/class", classRouter)
 app.use("/homework", hwRouter)
+
+
 
 app.use(cookieParser())
 
